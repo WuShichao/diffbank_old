9fef96d4b4feb39b6bbbe9f3ad30066d72f6dd76
Adam Coogan <dr.adam.coogan@gmail.com>
2021-06-17 12:23:00 +0200

    extend test_uniform

--- test/test_uniform.py
+++ test/test_uniform.py
@@ -1,4 +1,5 @@
 from math import sqrt
+import os
 
 import click
 import jax
@@ -40,15 +41,20 @@ def get_eff(x, y):
     return 1 - jnp.sum((x - y) ** 2)
 
 
-def _run(key, m_max, eta, mm, plot):
+def _run(key, m1_max, m2_max, eta, mm, plot, suffix="", save_dir=""):
     key = random.PRNGKey(key)
-    m_min, m_max = jnp.array(0.0), jnp.array(m_max)
-    naive_vol = (m_max - m_min) ** 2
+    m_min, m1_max, m2_max = jnp.array(0.0), jnp.array(m1_max), jnp.array(m2_max)
+    naive_vol = (m1_max - m_min) * (m2_max - m_min)
     m_star = 1 - mm  # maximum mismatch
 
     # Uniform sampler
     def sampler(key, n):
-        return random.uniform(key, (n, 2), minval=m_min, maxval=m_max)
+        return random.uniform(
+            key,
+            (n, 2),
+            minval=jnp.stack([m_min, m_min]),
+            maxval=jnp.stack([m1_max, m2_max]),
+        )
 
     def is_in_bounds(theta):
         m1, m2 = theta
@@ -56,21 +62,21 @@ def _run(key, m_max, eta, mm, plot):
             m1 < m_min,
             jnp.array(0.0),
             jnp.where(
-                m1 > m_max,
+                m1 > m1_max,
                 jnp.array(0.0),
                 jnp.where(
                     m2 < m_min,
                     jnp.array(0.0),
-                    jnp.where(m2 > m_max, jnp.array(0.0), jnp.array(1.0)),
+                    jnp.where(m2 > m2_max, jnp.array(0.0), jnp.array(1.0)),
                 ),
             ),
         )
 
     # Correct for boundary effects
     key, pts_key, ell_key = random.split(key, 3)
-    pts_ib = sampler(pts_key, 200)
+    pts_ib = sampler(pts_key, 1000)
     frac_in_bounds, frac_in_bounds_err = get_template_frac_in_bounds(
-        ell_key, pts_ib, get_g, m_star, is_in_bounds, 10
+        ell_key, pts_ib, get_g, m_star, is_in_bounds, 100
     )
     assert frac_in_bounds_err / frac_in_bounds < 0.1
     print(
@@ -96,11 +102,12 @@ def _run(key, m_max, eta, mm, plot):
     n_templates_Zn = int(naive_vol / (2 * m_star / sqrt(2)) ** 2)
     print(f"{n_templates_Zn} templates required for a Z_n lattice")
 
+    # Create the bank
     key, subkey = random.split(key)
     templates = sampler(subkey, n_templates)
 
     # Compute effectualness
-    n_test = 250
+    n_test = 500
     key, subkey = random.split(key)
     points = sampler(subkey, n_test)
     effectualnesses = np.zeros(n_test)
@@ -115,6 +122,9 @@ def _run(key, m_max, eta, mm, plot):
 
     # Test that the bank was generated consistently
     assert eff_frac - 5 * eff_frac_err < eta and eta < eff_frac + 5 * eff_frac_err
+    print(
+        f"Effectualness: [{eff_frac - 2 * eff_frac_err}, {eff_frac + 2 * eff_frac_err}]"
+    )
 
     if plot:
         # Plot
@@ -126,7 +136,10 @@ def _run(key, m_max, eta, mm, plot):
         plt.axvline(mm, color="r")
         plt.xlabel("Effectualness")
         plt.ylabel("Frequency")
-        plt.title(r"$%.3f \pm %.3f$ above %g" % (eff_frac, eff_frac_err, m_star))
+        plt.title(
+            f"[{eff_frac - 2 * eff_frac_err:.3f}, {eff_frac + 2 * eff_frac_err:.3f}]"
+            f" above {m_star:.3f}"
+        )
 
         # Templates
         plt.subplot(1, 2, 2)
@@ -136,32 +149,56 @@ def _run(key, m_max, eta, mm, plot):
         circles = PatchCollection(circles, zorder=0, alpha=0.2)
         plt.gca().add_collection(circles)
         plt.scatter(*templates.T, s=15, c="C1", marker="x")
+
+        # Effectualness points
         plt.scatter(*points.T, s=1, c="r")
-        plt.xlim(m_min, m_max)
-        plt.ylim(m_min, m_max)
+
+        # Boundary
+        plt.plot(
+            [m_min, m1_max, m1_max, m_min, m_min],
+            [m_min, m_min, m2_max, m2_max, m_min],
+            "k",
+            linewidth=0.5,
+        )
+
+        plt.xlim(m_min - 0.5, m1_max + 0.5)
+        plt.ylim(m_min - 0.5, m2_max + 0.5)
         plt.xlabel(r"$x_1$")
         plt.ylabel(r"$x_2$")
         plt.title("Template bank")
-        plt.gca().set_aspect("equal")
+        # plt.gca().set_aspect("equal")
 
         plt.suptitle(r"$^{%g}\mathcal{R}_{%i}(%g)$" % (eta, 2, mm))
         plt.tight_layout()
-        plt.savefig("test-uniform.png")
+        os.makedirs(save_dir, exist_ok=True)
+        fig_path = os.path.join(save_dir, f"test-uniform{suffix}.png")
+        plt.savefig(os.path.join(save_dir, fig_path))
+        print(f"Saved figure to {fig_path}")
 
 
 def test():
-    _run(45389, 2.0, 0.99, 0.95, False)
+    _run(45389, 2.0, 2.0, 0.99, 0.95, False)
 
 
 @click.command()
 @click.option("--key", default=8539, help="PRNG key")
-@click.option("--m_max", default=1.5, help="maximum parameter value")
+@click.option("--m1_max", default=1.5, help="maximum parameter value")
+@click.option("--m2_max", default=1.5, help="maximum parameter value")
 @click.option("--eta", default=0.99)
 @click.option("--mm", default=0.9, help="minimum match")
 @click.option("--plot/--no-plot", default=False, help="save plot")
-def run(key, m_max, eta, mm, plot):
-    _run(key, m_max, eta, mm, plot)
+@click.option("--suffix", default="", help="suffix for plot filename")
+@click.option("--save-dir", default="", help="directory in which to save plot")
+def run(key, m1_max, m2_max, eta, mm, plot, suffix, save_dir):
+    if suffix != "":
+        plot = True
+    _run(key, m1_max, m2_max, eta, mm, plot, suffix, save_dir)
 
 
 if __name__ == "__main__":
-    run()
+    m1_max = 100.0
+    m2_max = 0.05
+    save_dir = f"{m1_max:g}x{m2_max:g}"
+    for key in [0]:  # jnp.arange(0, 10, 1, dtype=int):
+        suffix = f"-{m1_max:g}x{m2_max:g}-{key}"
+        _run(key, m1_max, m2_max, 0.99, 0.95, True, suffix, save_dir)
