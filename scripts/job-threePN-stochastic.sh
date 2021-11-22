568fdd68629d832b46b27a7a546b4be9660224a4
Adam Coogan <dr.adam.coogan@gmail.com>
2021-11-22 13:09:24 -0500

    make jobs executable

--- scripts/job-threePN-stochastic.sh
+++ scripts/job-threePN-stochastic.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=threePN
+#SBATCH --job-name=3pn-stochastic
 #SBATCH --account=rrg-lplevass
-#SBATCH --time=00-03:00:00
+#SBATCH --time=00-24:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=47000M
 
 source ~/.virtualenvs/diffbank-3.9.6/bin/activate
 
-python genbank_2D_threePN.py --seed 1 --kind random --device gpu
+python genbank_2D_threePN.py --seed 1000 --kind stochastic --device gpu --n-eff 500 --noise analytic
